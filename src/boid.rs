c8e16eb6a80dda3134122032ebfb55ec74d524d3
Metamate <jakikob@gmail.com>
2022-05-17 15:26:46 +0200

    add align but it acts weird

--- src/boid.rs
+++ src/boid.rs
@@ -1,5 +1,10 @@
+use std::ops::Sub;
+
 use nannou::prelude::*;
 
+use crate::Model;
+
+#[derive(PartialEq, Copy, Clone)]
 pub struct Boid {
     pub position: Vec2,
     pub velocity: Vec2,
@@ -17,14 +22,21 @@ impl Boid {
         }
     }
 
-    pub fn draw(&self, draw: &Draw) {
+    pub fn update(&mut self, app: &App, boids: Vec<Boid>) {
+        self.position += self.velocity;
+        self.velocity += self.acceleration;
+        self.flock(boids);
+        self.contain(app);
+    }
+
+    pub fn show(&self, draw: &Draw) {
         draw.ellipse()
             .color(self.color)
             .w_h(10., 10.)
             .x_y(self.position.x, self.position.y);
     }
 
-    pub fn contain(&mut self, app: &App) {
+    fn contain(&mut self, app: &App) {
         let boundary = app.window_rect().l_r_b_t();
         if self.position.x > boundary.1 {
             self.position.x = boundary.0;
@@ -35,6 +47,32 @@ impl Boid {
             self.position.y = boundary.2;
         } else if self.position.y < boundary.2 {
             self.position.y = boundary.3;
+        }
+    }
+
+    pub fn flock(&mut self, boids: Vec<Boid>) {
+        let alignment = self.align(boids);
+        self.acceleration = alignment;
+    }
+
+    fn align(&mut self, boids: Vec<Boid>) -> Vec2 {
+        let perception_radius = 10.;
+        let local_boids: Vec<Boid> = boids
+            .into_iter()
+            .filter(|boid| {
+                self.position.distance(boid.position) < perception_radius && boid != self
+            })
+            .collect();
+
+        if local_boids.len() > 0 {
+            let steering = local_boids
+                .iter()
+                .fold(Vec2::default(), |sum, boid| sum + boid.velocity)
+                / local_boids.len() as f32;
+            self.velocity -= steering;
+            return steering;
+        } else {
+            Vec2::ZERO
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,23 @@ fn model(app: &App) -> Model {
     let _window = app.new_window().view(view).build().unwrap();
     let mut boids = Vec::new();
 
+    let boundary = app.window_rect().l_r_b_t();
     for _n in 0..NO_OF_BOIDS {
-        boids.push(spawn_boid(app));
+        let random = (
+            random_range(boundary.0, boundary.1),
+            random_range(boundary.2, boundary.3),
+        );
+        let boid = Boid::new(random.0, random.1);
+        boids.push(boid);
     }
 
     Model { _window, boids }
 }
 
-fn update(app: &App, _model: &mut Model, _update: Update) {
-    for boid in _model.boids.iter_mut() {
-        boid.position += boid.velocity;
-        boid.contain(app);
+fn update(app: &App, model: &mut Model, _update: Update) {
+    let boids = model.boids.clone();
+    for boid in model.boids.iter_mut() {
+        boid.update(app, boids.clone());
     }
     println!("{:?}", 1. / _update.since_last.as_secs_f64());
 }
@@ -36,17 +42,7 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BEIGE);
     for boid in _model.boids.iter() {
-        boid.draw(&draw);
+        boid.show(&draw);
     }
     draw.to_frame(app, &frame).unwrap();
-}
-
-fn spawn_boid(app: &App) -> Boid {
-    let boundary = app.window_rect().l_r_b_t();
-    let random = (
-        random_range(boundary.0, boundary.1),
-        random_range(boundary.2, boundary.3),
-    );
-    let boid = Boid::new(random.0, random.1);
-    boid
 }
